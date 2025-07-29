9386d2c5879f8a534d212a7a186ff076fd070b78
waliqs <>
2025-07-29 14:00:24 +0500

    add for docker and ancible file

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from app import db
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Post=Post)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
